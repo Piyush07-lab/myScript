11f6ccbb5337dd4a6d7b21c524b1bcc1a7193723
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-05 15:28:49 +0530

    Add conversion operations and string concatenation examples

---
+++ 01_basics/03conv_op.js
@@ -0,0 +1,33 @@
+let score = "33abc"
+
+
+
+// console.log(typeof score)
+// console.log(typeof(score)) 
+
+// let valueInNumber = Array(score) 
+// console.log(typeof valueInNumber) >> Object
+// console.log(valueInNumber)  >> The obvious
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) >> Number
+// console.log(valueInNumber) >> NaN
+
+// let valueInNumber = String(score)
+// console.log(typeof valueInNumber) >> String
+// console.log(valueInNumber) >> "33abc"
+
+let x = 4;
+let y = ++x;
+console.log(y);
+
+// ******************* Operation ******************
+
+let str1 = "Hello"
+let str2 = "Piyush"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+
