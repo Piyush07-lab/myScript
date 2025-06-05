2b666460fc77c92208fd091477a05387abe99e05
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-05 19:13:18 +0530

    Refactor variable declarations for clarity and consistency

--- 01_basics/01variable.js
+++ 01_basics/01variable.js
@@ -1,11 +1,13 @@
 const accountId = 22469  // Declaring constant means the data is non changable
 let accountEmail = "piyushmishraone999@gmail.com"
-var accountPassword = "12345"    //Some good guy said prefer not to use var coz of the issue in block and scope function
-accountCity = "Jaipur"     //yeah so this works but is not optimal
+var accountPassword = "12345"    // var is also mutable, but it's not recommended to use it in modern JavaScript, it has function scope, meaning it can be accessed outside the block where it was defined reasulting in potential bugs in larger codebases.
+let accountCity = "Jaipur"     // let and var are mutable, meaning we can change their content later
 let accountState       //undefined
 
-// accountId = 2    can't change contant now, can we?
+// accountId = 2    can't change content now, can we?
 
 accountEmail = "theone1117@outlook.com"
 accountPassword = "67890"
 accountCity = "Jhumritalaiya"
+
+console.table([accountId, accountEmail, accountPassword, accountState, accountCity])
