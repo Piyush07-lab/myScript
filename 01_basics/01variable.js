58dec5e50ab7f6295cb633934b5fe98a0e1120ae
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-05 11:17:16 +0530

    Add initial JavaScript files for variable declaration and data types

---
+++ 01_basics/01variable.js
@@ -0,0 +1,11 @@
+const accountId = 22469  // Declaring constant means the data is non changable
+let accountEmail = "piyushmishraone999@gmail.com"
+var accountPassword = "12345"    //Some good guy said prefer not to use var coz of the issue in block and scope function
+accountCity = "Jaipur"     //yeah so this works but is not optimal
+let accountState       //undefined
+
+// accountId = 2    can't change contant now, can we?
+
+accountEmail = "theone1117@outlook.com"
+accountPassword = "67890"
+accountCity = "Jhumritalaiya"
---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,19 @@
+// alert("hello")/
+
+console.log(3+3)
+
+let name = "Piyush"  //String
+let age = 18  //numrical
+let isLoggedIn = false   //Boolean
+
+// number => 2 to power 53
+// bigint => 2 to power 53 and above, used for large integers
+// string => sequence of characters, can be single or double quoted
+// boolean => true/false
+// null => represents no value, empty value
+// undefined => variable that has been declared but not assigned a value 
+// symbol => unique identifier, used for object properties
+
+// object => key-value pairs, mutable, reference type
+
+console.log(typeof age)
