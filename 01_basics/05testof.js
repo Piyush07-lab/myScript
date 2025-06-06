29b0019d0969fdc424d164d6d5f515d45136cb0d
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-06 16:55:28 +0530

    Add functions for square and cube calculations with console output

---
+++ 01_basics/05testof.js
@@ -0,0 +1,18 @@
+//functions
+//------Square-----
+function squareOf(x) {
+    return x * x;
+}
+//------cube------
+function cubeOf(x) {
+    return x * x * x;
+}
+
+console.log(cubeOf(2));
+console.log(squareOf(9));
+
+//conditionals
+
+
+
+
