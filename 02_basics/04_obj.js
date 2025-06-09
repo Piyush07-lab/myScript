4a836b1ad4be3b79ec23b7350d1433df18fce024
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-10 00:02:21 +0530

    Format object declarations for consistency and improve readability

--- 02_basics/04_obj.js
+++ 02_basics/04_obj.js
@@ -23,12 +23,21 @@ const tinderUser2 = {
 
 console.log(tinderUser2.fullname.userfullname.firstName); // Accessing nested properties in the object
 
-const obj1 = {1: "a", 2: "b", 3: "c"}; // Object with numeric keys
-const obj2 = {4: "x", 5: "y", 6: "z"}; // Object with string keys 
+const obj1 = {
+    1: "a", 
+    2: "b", 
+    3: "c"
+}; 
+const obj2 = {
+    4: "x", 
+    5: "y", 
+    6: "z"
+};  
 
 const obj3 = {...obj1, ...obj2}; // Merging two objects using spread operator
 
 const obj4 = Object.assign({}, obj1, obj2); // Merging two objects using Object.assign
 
-console.log(obj3); // Output: {1: "a", 2: "b", 3: "c", 4: "x", 5: "y", 6: "z"}
-console.log(obj4); // Output: {1: "a", 2: "b", 3: "c", 4: "x", 5: "y", 6: "z"}
+console.log(obj3); 
+
+console.log(obj4); 
