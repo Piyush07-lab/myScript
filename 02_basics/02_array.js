66729fb965d5c1f37078a543028beb624a89ac3e
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-09 20:44:25 +0530

    Refactor array examples and add additional console outputs for clarity

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -24,4 +24,20 @@ const anotherOne = [1, 2, 3, [4, 5, 6, [7, 8]]] // This is a nested array with m
 
 const flatArray = anotherOne.flat(2); // This will flatten the array to a depth of 2. We could also use flat(Infinity) to flatten it completely. but it is not recommended as it can cause performance issues.
 
-console.log("Flat Array: ", flatArray); // This will print the flattened array.
+// console.log("Flat Array: ", flatArray); // This will print the flattened array.
+
+// console.log(Array.isArray("Piyush"));
+// This will check if the given value is an array or not. It will return false as "Piyush" is a string.
+
+// console.log(Array.from("Piyush")); // This will convert the string "Piyush" into an array of characters. It will return ['P', 'i', 'y', 'u', 's', 'h'].
+// console.log(Array.from({name: "Piyush"})); // Empty array as the object is not iterable. It will return []. We'll see why later.
+
+let score1 = 87
+let score2 = 93
+let score3 = 89
+
+console.log("All the Scores", Array.of(score1, score2, score3)); // This will create an array with the given values. It will return [87, 93, 89].
+
+
+
+
