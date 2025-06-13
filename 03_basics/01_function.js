1b7564f38c3625ef8ca92ebf940e7a88940e4db1
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-13 11:05:57 +0530

    Enhance function implementations and add object handling examples; update launch configuration for debugging

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -9,11 +9,46 @@ function sayMyName(){
     return `You're ${name} ${lastname}!`
 }
 
-console.log(sayMyName());
+// console.log(sayMyName());
 
 
 function sumOf(num1, num2){
-    console.log(num1 + num2);
+    return num1 + num2
 }
 
-sumOf(7476473, 67647741983)
+const result = sumOf(20, 4)
+
+// console.log("Result: ", result); // Or I could just declare result variable within the scope of the function before rturning it. Depends on the need.
+
+// function loginUserMessage(userName){
+//     if(userName === undefined){
+//         console.log("Please enter a username");
+//         return
+//     }
+//     return `${userName} just logged in!`
+// } // This works obv but it's not used coz big argument inside and me lazy.
+
+// function loginUserMessage(userName = ""){
+//     return `${userName} just logged in!`
+// } // This is what I would do coz it's less typing.
+
+
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(...num1){
+    return num1    // [Rest operator]
+}
+
+console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    userName: "piyush",
+    id: "A0240764"
+}
+
+function handleObj(anyobject) {
+    console.log(`Username is ${anyobject.userName}, and their Id is ${anyobject.id}`);
+}
+
+handleObj(user)
