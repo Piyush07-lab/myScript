ceffe7a8d2ed82ff1185ca057c06c0acd2209a42
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-13 22:39:11 +0530

    Comment out code examples for clarity and maintainability in 02_scope.js

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -2,26 +2,51 @@
 
 // var c = 300 this will get mutated coz th var declared below inside the block
 
-let a = 300 // this is outside of the scope so reusing this variable is not a problem
+// let a = 300 // this is outside of the scope so reusing this variable is not a problem
 
 // Same can't be said about var tho
 
-if (true) {
+// if (true) {
 
-    let a = 10
-    const b = 20
-    console.log("inner: ", a);
+    // let a = 10
+    // const b = 20
+    // console.log("inner: ", a);
     
     // var c = 30    average red button
     
-}
+// }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+
+    // const element = array[i];
     
-}
+// }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// nested scope
+
+// function one(){
+//     const userName = "Piyush"
+
+//     function two(){
+//         const website = "YouTube" 
+//         console.log(userName); // Child function can use elements from parent function, coz obv that's like the global scope.
+        
+//     }
+//     console.log(website); // But parent function can not use child's elements [it will throw a not defined error]
+
+//     two()
+// }
+
+// one() // this entire shit is apparently called closure.
+
+
+if (true){
+    const username = "piyush"
+    if (username ==="piyush") {
+        const tagline = "707"
+        console.log(username + tagline);
+    }
+    // console.log(website); // we already know it...
+}
+// console.log(username);
 
