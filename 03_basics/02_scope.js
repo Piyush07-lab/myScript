7448fc5d36c6e051744c6d4a8b71caf21264e69c
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-13 16:32:12 +0530

    Refactor variable declarations and comments for clarity in 02_scope.js

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,8 +1,25 @@
 // Scope of a function
 
-let a = 10
-const b = 20
-var c = 30
+// var c = 300 this will get mutated coz th var declared below inside the block
+
+let a = 300 // this is outside of the scope so reusing this variable is not a problem
+
+// Same can't be said about var tho
+
+if (true) {
+
+    let a = 10
+    const b = 20
+    console.log("inner: ", a);
+    
+    // var c = 30    average red button
+    
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
 
 console.log(a);
 console.log(b);
