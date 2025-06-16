877e57b6f8039ac931dc7c9c22df9a857b58186d
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-16 12:39:20 +0530

    Refactor IIFE examples for clarity and consistency; add semicolon note

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -9,10 +9,19 @@
 // Syntax
 
 (function chai(){
+    // Named IIFE
     console.log(`DB CONNECTED`);
     
-})()
+})();
 // It's basically a fumction which we don't need to call. It would function automatically on it's own
 // So iife in technicality is function calling itself without any calls, by nesting itself inside a parenthesis and using a function call parenthesis.
 // (In-Interview) IIFE is used to invoke a function immediately and also because of global scopes, a lot of variables and declaration pollute the code space. To get rid of that we use iife. 
+// Also very importantly, you have to use semicolon coz this type of function don't know when to end
+
+( (vers) => {
+    // Unnamed IIFE
+    console.log(`DB Connected ${vers}`);
+}) ("20.3");
+
+
 
