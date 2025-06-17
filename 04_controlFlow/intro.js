636faca8fd9965a6385caf13f5752541e9b5ce17
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-17 10:14:12 +0530

    Add examples and explanations to intro.js and one.js for better clarity on control flow

--- 04_controlFlow/intro.js
+++ 04_controlFlow/intro.js
@@ -1,4 +1,13 @@
 /*-------------->Introduction<------------------*/
 /*
 When run a code snippet, the entirity of it gets executed. All of it! But that is not at all efficient to run the entire snippet and thats why we use conditionals or loops to get what we want efficiently. These are called control flow. Control of the flow of code or Let code flow with complete control. 
+
+Examples of these functions are 
+
+if (conditionals) {
+    
+}
+
+
 */
+
---
+++ 04_controlFlow/one.js
@@ -0,0 +1,17 @@
+// If
+
+// if (true) {
+//     return "something"
+// }
+
+ // Example 
+
+ const isLoggedIn = true
+
+ if (isLoggedIn == true) {
+    console.log("User is logged in");
+ } // this is one way to do it, but ofc there are more ways.
+
+ // We can check for true or false in more than one way which depends on what kind of project we are making and how much time we have.
+
+ 
