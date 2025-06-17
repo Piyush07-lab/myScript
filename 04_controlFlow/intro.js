43b1dc92c827e4fe88e5b03dbb40ab9a0c3247df
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-17 10:01:41 +0530

    Add introductory comments to intro.js and remove empty one.js file

---
+++ 04_controlFlow/intro.js
@@ -0,0 +1,4 @@
+/*-------------->Introduction<------------------*/
+/*
+When run a code snippet, the entirity of it gets executed. All of it! But that is not at all efficient to run the entire snippet and thats why we use conditionals or loops to get what we want efficiently. These are called control flow. Control of the flow of code or Let code flow with complete control. 
+*/
