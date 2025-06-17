d6139706c3da71ce2c25a7012851c66ca6d461f8
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-17 09:50:13 +0530

    Enhance explanation of execution context and call stack in 05_exc.js

--- 03_basics/05_exc.js
+++ 03_basics/05_exc.js
@@ -27,6 +27,11 @@ Now, What happens when we run the code above? A seiries of operations will happe
 
 Memory creation phase or memory phase is where all the variables and the function and it's definition is allocated a memory location but they are not assigned values. (undefined)
 Execution phase is where the value is assigned to the variables. But everytime a function is called it creates a new variable and thus it goes through both those phase again. 
-It happens evrytime a function is called.  
+It happens evrytime a function is called.
+So that means in the context of the code above line 8 will trigger a new variable execution environment and an execution thread. Same will happen in line 13 amd 14 too. Once it's done NVE will get deleted.
+
+>> Now What is call stack? 
+- We already know about the stack memory, then all we need to know is, it is a stack memory for calls. It stores calls and releases them in order of FILO (first in last out).
+Which ofc means that in the case of the code above, line 13 and 14 will run first then it will reference to line 8 to 11, so that will run next an dthen finally line 5 and 6. Coz that is where the value was declared and it was reffered by the function in line 13. And I know this seems confusing but is not since function in line 13 and 14 has diferent execution environment wich will run simulteneously. Hope that explains everything. And I did not include Global EC because it is there always in the beggining 
 
 */
